057472223fdb6bffad3a7d024b6f0cd1d5954b13
leslie-fang <fangjiahao@sjtu.edu.cn>
2017-04-14 23:53:45 +0800

    init

---
+++ polls/views.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.template import loader
+import django.contrib.staticfiles
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def main(request):
+    template = loader.get_template('polls/main.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+    #return HttpResponse('Heelo ')
